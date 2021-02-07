a6b35f4cff2ac65ac58657b33378d19b659ba696
Renu Punjabi <rpunjabi12@gmail.com>
2021-02-07 14:02:15 -0800

    Removed storyboard
    SceneDelegate set up working. Removed storyboard

---
+++ Image Carousel/Coordinators/AppCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  AppCoordinator.swift
+//  Image Carousel
+//
+//  Created by Renu Punjabi on 2/7/21.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+    func start()
+    func stop()
+//    var presenterViewController: UIViewController { get }
+//    var navigationViewController: UINavigationController { get }
+}
+
+final class AppCoordinator: Coordinator {
+        
+    let window: UIWindow
+    
+    init(_ window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let imagesViewController = ImagesCollectionViewController()
+       let navigationViewController = UINavigationController(rootViewController: imagesViewController)
+        window.rootViewController = navigationViewController
+        window.makeKeyAndVisible()
+    }
+    
+    func stop() { }
+
+    
+}
---
+++ Image Carousel/ViewControllers/ImagesCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesCollectionViewController.swift
+//  Image Carousel
+//
+//  Created by Renu Punjabi on 2/7/21.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesCollectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
